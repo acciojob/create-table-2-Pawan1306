0f8b8a7f7add75a75dc9995ff5d9b4c06141dbf5
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-11-05 20:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 function createTable() {
-    //Write your code here
-  
+  // Prompt for the number of rows and columns
+  const rn = parseInt(prompt("Input number of rows"), 10);
+  const cn = parseInt(prompt("Input number of columns"), 10);
+
+  // Get the element with id "myTable"
+  const myTable = document.getElementById("myTable");
+
+  // Create a new HTML table
+  const table = document.createElement("table");
+
+  // Loop to create rows and columns
+  for (let i = 0; i < rn; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < cn; j++) {
+      const cell = document.createElement("td");
+      cell.textContent = `Row-${i} Column-${j}`;
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+
+  // Clear the existing content of "myTable" and append the new table
+  myTable.innerHTML = '';
+  myTable.appendChild(table);
 }
+
